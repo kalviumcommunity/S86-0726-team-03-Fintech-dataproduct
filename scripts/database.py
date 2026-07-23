423a1ecf235e01276c15913793b4c72254eb1635
Sri Charan <charan.bukka@gmail.com>
2026-07-23 14:47:51 +0530

    Solved the duplicate codes for joining of the tables and also cleaned the three seperate files of the CSV seperately and joined it using SQL

--- scripts/database.py
+++ scripts/database.py
@@ -8,10 +8,7 @@ DATABASE_URL = "sqlite:///analytics.db"
 engine = create_engine(DATABASE_URL)
 
 
-def save_to_database(df):
-    """
-    Save cleaned DataFrame to SQLite database.
-    """
+def save_to_database(df, payment_retries, bank_responses):
 
     df.to_sql(
         "transactions_cleaned",
@@ -20,8 +17,21 @@ def save_to_database(df):
         index=False
     )
 
-    print("\nCleaned data successfully saved to SQLite database.")
+    payment_retries.to_sql(
+        "payment_retries",
+        engine,
+        if_exists="replace",
+        index=False
+    )
 
+    bank_responses.to_sql(
+        "bank_response_codes",
+        engine,
+        if_exists="replace",
+        index=False
+    )
+
+    print("\nAll cleaned tables successfully saved to SQLite.")
 
 def load_from_database():
     """
@@ -29,8 +39,29 @@ def load_from_database():
     """
 
     query = """
-    SELECT *
-    FROM transactions_cleaned
+    SELECT
+        t."Transaction ID",
+        t."Timestamp",
+        t."Sender Name",
+        t."Sender UPI ID",
+        t."Receiver Name",
+        t."Receiver UPI ID",
+        t."Amount (INR)",
+        t."Status",
+        t."Is_Outlier",
+        t."Valid_Amount",
+        t."Valid_Status",
+        t."Valid_Sender_UPI",
+        t."Valid_Receiver_UPI",
+        t."Passes_All_Checks",
+        p."Retry Count",
+        b."Response Code",
+        b."Response Description"
+    FROM transactions_cleaned t
+    LEFT JOIN payment_retries p
+        ON t."Transaction ID" = p."Transaction ID"
+    LEFT JOIN bank_response_codes b
+        ON t."Transaction ID" = b."Transaction ID"
     """
 
     df = pd.read_sql(query, engine)
